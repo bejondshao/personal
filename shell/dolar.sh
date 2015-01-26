69f163a6ab7d7d683839e60ab2f2941ae38f955d
Bejond Shao <bejond.shao@liferay.com>
2015-01-27 00:46:10 +0800

    PPS-1 bash test

---
+++ shell/dolar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# $ is used for variate and for current process id
+
+word="goodboy"
+echo "$word"
+echo "Current pid is: $$"
---
+++ shell/read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Please enter a sentence."
+read sentence
+echo "Your input: $sentence"
