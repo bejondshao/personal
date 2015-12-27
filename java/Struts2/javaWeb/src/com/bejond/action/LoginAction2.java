1c1c026fda54f28087afcb386b8ccd418b7448a5
Bejond Shao <bejond@163.com>
2015-12-27 14:01:14 +0800

    struts2-action 依赖注入, most popular way

--- java/Struts2/javaWeb/src/com/bejond/action/LoginAction2.java
+++ java/Struts2/javaWeb/src/com/bejond/action/LoginAction2.java
@@ -2,21 +2,38 @@ package com.bejond.action;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.RequestAware;
+import org.apache.struts2.interceptor.SessionAware;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by bejond on 15-12-27.
  */
-public class LoginAction2 extends ActionSupport {
-    private Map request;
-    private Map session;
-    private Map application;
+public class LoginAction2 extends ActionSupport implements RequestAware, SessionAware, ApplicationAware {
+    private Map<String, Object> request;
+    private Map<String, Object> session;
+    private Map<String, Object> application;
 
-    public LoginAction2() {
+    /*public LoginAction2() {
         request = (Map)ActionContext.getContext().get("request");
         session = ActionContext.getContext().getSession();
         application = ActionContext.getContext().getApplication();
+    }*/
+
+    @Override
+    public void setRequest(Map<String, Object> request) {
+        this.request = request;
+    }
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+    @Override
+    public void setApplication(Map<String, Object> application) {
+        this.application = application;
     }
 
     public String execute() {
@@ -26,4 +43,6 @@ public class LoginAction2 extends ActionSupport {
 
         return SUCCESS;
     }
+
+
 }
