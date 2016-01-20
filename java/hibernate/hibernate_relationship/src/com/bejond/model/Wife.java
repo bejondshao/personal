3a6c1dd542ec12187686ac4cb96d83c048540c5b
Bejond Shao <bejond.shao@liferay.com>
2016-01-20 11:39:41 +0800

    hibernate bi_fk annotation config

--- java/hibernate/hibernate_relationship/src/com/bejond/model/Wife.java
+++ java/hibernate/hibernate_relationship/src/com/bejond/model/Wife.java
@@ -3,6 +3,8 @@ package com.bejond.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 /**
  * Created by bejond on 16-1-18.
@@ -11,6 +13,16 @@ import javax.persistence.Id;
 public class Wife {
 	private int wifeId;
 	private String name;
+	private Husband husband;
+
+	@OneToOne (mappedBy = "wife")
+	public Husband getHusband() {
+		return husband;
+	}
+
+	public void setHusband(Husband husband) {
+		this.husband = husband;
+	}
 
 	@Id
 	@GeneratedValue
