9f1e44ebee78be43b8ba37ced9fc251de4207168
Bejond Shao <bejond.shao@liferay.com>
2016-01-21 13:54:53 +0800

    hibernate one2many, many2one

---
+++ java/hibernate/hibernate_bi_one_to_many_many_to_one/src/com/bejond/hibernate/model/Group.java
@@ -0,0 +1,45 @@
+package com.bejond.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by bejond on 16-1-21.
+ */
+@Entity(name = "group_")
+public class Group {
+	private int groupId;
+	private String groupName;
+	private Set<User> userSet = new HashSet<User>();
+
+	@OneToMany(mappedBy = "group")
+	public Set<User> getUserSet() {
+		return userSet;
+	}
+
+	public void setUserSet(Set<User> userSet) {
+		this.userSet = userSet;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+}
---
+++ java/hibernate/hibernate_bi_one_to_many_many_to_one/src/com/bejond/hibernate/model/User.java
@@ -0,0 +1,45 @@
+package com.bejond.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * Created by bejond on 16-1-21.
+ */
+@Entity
+public class User {
+	private int userId;
+	private String userName;
+	private Group group;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "groupId")
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+}
