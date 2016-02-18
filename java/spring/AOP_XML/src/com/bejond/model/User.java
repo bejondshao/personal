f1db07a6abea3f24e02f18c31351df91f315a8d9
Bejond Shao <bejond.shao@liferay.com>
2016-02-18 11:57:07 +0800

    spring hibernate

--- java/spring/AOP_XML/src/com/bejond/model/User.java
+++ java/spring/AOP_XML/src/com/bejond/model/User.java
@@ -1,14 +1,32 @@
 package com.bejond.model;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by bejond on 16-2-3.
  */
 @Component("user")
 @Scope("prototype")
+@Entity
 public class User {
+	private int id;
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	private String username;
 	private String password;
 
