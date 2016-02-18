0adcf0fe45f888d2d8180ff89ce28c9452c480d5
Bejond Shao <bejond.shao@liferay.com>
2016-02-18 13:40:57 +0800

    spring hibernate save

--- java/spring/AOP_XML/src/com/bejond/DAO/impl/UserDAOImpl.java
+++ java/spring/AOP_XML/src/com/bejond/DAO/impl/UserDAOImpl.java
@@ -2,6 +2,8 @@ package com.bejond.DAO.impl;
 
 import com.bejond.DAO.UserDAO;
 import com.bejond.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -13,27 +15,29 @@ import java.sql.Connection;
  */
 @Component("userDAOImpl")
 public class UserDAOImpl implements UserDAO {
-	private DataSource dataSource;
-
-	public DataSource getDataSource() {
-		return dataSource;
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
 	}
 
 	@Resource
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
+
+	private SessionFactory sessionFactory;
 
 	@Override
 	public void save(User user) {
-		Connection connection = null;
 		try {
-			connection = dataSource.getConnection();
-			connection.createStatement().executeUpdate(
-				"insert into user values (null, 'aa', 'password')");
+			Session session = sessionFactory.openSession();
+			session.beginTransaction();
+			user.setUsername("Zhangfei");
+			user.setPassword("test");
+			session.save(user);
+			session.getTransaction().commit();
 
-			connection.close();
 			System.out.println("UserDAO.save()");
+			session.close();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
