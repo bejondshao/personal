2a7919c1c00b5fec636d234c455a5b5a8ea2241d
Bejond Shao <bejond.shao@liferay.com>
2016-02-18 10:34:32 +0800

    spring dbcp

---
+++ java/spring/AOP_XML/src/com/bejond/AOP/DataSourceAspect.java
@@ -0,0 +1,84 @@
+package com.bejond.AOP;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+
+import javax.jws.WebMethod;
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.*;
+import java.util.logging.Logger;
+
+/**
+ * Created by bejond on 16-2-17.
+ */
+public class DataSourceAspect implements DataSource {
+	@Override
+	public Connection getConnection() throws SQLException {
+		return null;
+	}
+
+	@Override
+	public Connection getConnection(String username, String password) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		return null;
+	}
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+
+	}
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+
+	}
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		return 0;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return null;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return false;
+	}
+
+	public Object aroundMethod(ProceedingJoinPoint proceedingJoinPoint)
+		throws Throwable {
+
+		Connection connection = getConnection();
+
+		Object returnValue = null;
+
+		try {
+			// start stopwatch
+			returnValue = proceedingJoinPoint.proceed();
+		}
+		catch (Exception e) {
+		}
+		finally {
+			// stop stopwatch
+			connection.close();
+		}
+
+		return returnValue;
+	}
+}
--- java/spring/AOP_XML/src/com/bejond/DAO/impl/UserDAOImpl.java
+++ java/spring/AOP_XML/src/com/bejond/DAO/impl/UserDAOImpl.java
@@ -4,15 +4,43 @@ import com.bejond.DAO.UserDAO;
 import com.bejond.model.User;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 /**
  * Created by bejond on 16-2-3.
  */
 @Component("userDAOImpl")
 public class UserDAOImpl implements UserDAO {
+	private DataSource dataSource;
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	@Resource
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Override
 	public void save(User user) {
-		System.out.println(user);
-		System.out.println("UserDAO.save()");
+		Connection connection = null;
+		try {
+			connection = dataSource.getConnection();
+			connection.createStatement().executeUpdate(
+				"insert into user values (null, 'aa', 'password')");
+
+			connection.close();
+			System.out.println("UserDAO.save()");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+
+		}
 	}
 
 	/*@Override
