3c8959efd14c468fb825af06e9e6d33a231b21af
Bejond Shao <bejond@163.com>
2016-02-22 22:46:19 +0800

    spring hibernateDaoSupport, improve, call super set method to set sessionFactory

--- java/spring/AOP_XML/src/com/bejond/DAO/impl/AbstractDAOImpl.java
+++ java/spring/AOP_XML/src/com/bejond/DAO/impl/AbstractDAOImpl.java
@@ -1,6 +1,7 @@
 package com.bejond.DAO.impl;
 
 import com.bejond.DAO.AbstractDAO;
+import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Component;
@@ -11,15 +12,9 @@ import javax.annotation.Resource;
  * Created by bejond on 2/21/16.
  */
 @Component
-public class AbstractDAOImpl implements AbstractDAO {
-	private HibernateTemplate hibernateTemplate;
-
-	public HibernateTemplate getHibernateTemplate() {
-		return hibernateTemplate;
-	}
-
-	@Resource
-	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-		this.hibernateTemplate = hibernateTemplate;
+public class AbstractDAOImpl extends HibernateDaoSupport implements AbstractDAO {
+	@Resource (name = "sessionFactory")
+	public void setAbstractSessionFactory(SessionFactory sessionFactory) {
+		super.setSessionFactory(sessionFactory);
 	}
 }
