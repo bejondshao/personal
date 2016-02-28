d4d830b8fadad5f80843de0a230758efa85e7039
Bejond Shao <bejond@163.com>
2016-02-28 12:10:04 +0800

    reg use HibernateTemplate can avoid add @Transactional in DAOImpl

--- java/spring/reg/src/com/bejond/DAO/impl/UserDAOImpl.java
+++ java/spring/reg/src/com/bejond/DAO/impl/UserDAOImpl.java
@@ -6,18 +6,19 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import javax.annotation.Resource;
 
 /**
  * Created by bejond on 2/24/16.
  */
 @Repository
-@Transactional
 public class UserDAOImpl implements UserDAO {
-	private SessionFactory sessionFactory;
+	/*private SessionFactory sessionFactory;
 
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
@@ -27,35 +28,57 @@ public class UserDAOImpl implements UserDAO {
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
+*/
+	private HibernateTemplate hibernateTemplate;
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	@Resource
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
 
 	/*@Override
-	public boolean isExisting(User user) {
-		Session session = sessionFactory.getCurrentSession();
-		session.beginTransaction();
-		String sqlQuery = "select count(*) from User where username = ?";
-		Query query = session.createQuery(sqlQuery);
-		query.setString(0, user.getUsername());
-		long count = (Long)query.uniqueResult();
+		public boolean isExisting(User user) {
+			Session session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			String sqlQuery = "select count(*) from User where username = ?";
+			Query query = session.createQuery(sqlQuery);
+			query.setString(0, user.getUsername());
+			long count = (Long)query.uniqueResult();
 
-		return count > 0;
-	}*/
+			return count > 0;
+		}*/
 	@Override
 	public User loadUserByName(String username) {
-		Session session = sessionFactory.getCurrentSession();
+		/*Session session = sessionFactory.getCurrentSession();
 		session.beginTransaction();
 		String sqlQuery = "select u from User u where username = ?";
 		Query query = session.createQuery(sqlQuery).setString(0, username);
 		User user = (User)query.uniqueResult();
 
-		return user;
+		return user;*/
+		List<User> userList = (List<User>)hibernateTemplate.findByNamedParam(
+			"from User u where u.username = :username", "username", username);
+
+		if (userList.size() > 0) {
+			return userList.get(0);
+		}
+
+		return null;
 	}
 
 	@Override
 	public User addUser(User user) {
-		Session session = sessionFactory.getCurrentSession();
+		/*Session session = sessionFactory.getCurrentSession();
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();
+
+		return user;*/
+		hibernateTemplate.save(user);
 
 		return user;
 	}
