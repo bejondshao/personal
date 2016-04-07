47e690744bf881b97aa7ba744d81c8ae0dcb81ab
Bejond Shao <bejond.shao@liferay.com>
2016-04-07 16:16:37 +0800

    test java 8 Interface Enhancement, default method && static method

---
+++ java/java8test/src/test/java/com/bejond/java8test/SubDefaultFunction.java
@@ -0,0 +1,20 @@
+package com.bejond.java8test;
+
+import org.junit.Test;
+
+/**
+ * Created by bejond on 16-4-7.
+ */
+public class SubDefaultFunction implements DefaultFunctionInterface {
+	@Test
+	public void testDefaultFunction() {
+		SubDefaultFunction subDefaultFunction = new SubDefaultFunction();
+		System.out.println(subDefaultFunction.count());
+	}
+
+	@Test
+	public void testStaticFunction() {
+		String name = DefaultFunctionInterface.findName(); // subclass can't inherit parent static method
+		System.out.println(name.replace('o', 'u'));
+	}
+}
