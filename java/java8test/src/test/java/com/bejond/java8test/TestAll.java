32ead7c3fd14e295c4327e3d8fb6529274f0ce52
Bejond Shao <bejond.shao@liferay.com>
2016-04-07 17:58:01 +0800

    test java 8 Collection Stream sequential && parallel

--- java/java8test/src/test/java/com/bejond/java8test/TestAll.java
+++ java/java8test/src/test/java/com/bejond/java8test/TestAll.java
@@ -2,6 +2,11 @@ package com.bejond.java8test;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
 /**
  * Created by bejond on 16-4-7.
  */
@@ -37,5 +42,33 @@ public class TestAll {
 				return string;
 			},
 			test);
+	}
+
+	@Test
+	public void testSequentialStream() {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < 1000000; i++) {
+			double d = Math.random() * 1000;
+			list.add(d + "");
+		}
+		long start = System.nanoTime();//获取系统开始排序的时间点
+		int count = (int)list.stream().sequential().sorted().count();
+		long end = System.nanoTime();//获取系统结束排序的时间点
+		long ms = TimeUnit.NANOSECONDS.toMillis(end - start);//得到串行排序所用的时间
+		System.out.println(ms + "ms");
+	}
+
+	@Test
+	public void testStreamParallel() {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < 1000000; i++) {
+			double d = Math.random() * 1000;
+			list.add(d + "");
+		}
+		long start = System.nanoTime();//获取系统开始排序的时间点
+		int count = (int)list.stream().parallel().sorted().count();
+		long end = System.nanoTime();//获取系统结束排序的时间点
+		long ms = TimeUnit.NANOSECONDS.toMillis(end - start);//得到并行排序所用的时间
+		System.out.println(ms + "ms");
 	}
 }
