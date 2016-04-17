b5043ddd7c2bd377ca741b2a7dd3a0d83a112b13
Bejond Shao <bejond@163.com>
2016-04-17 13:42:03 +0800

    js Date

---
+++ frontend/javascript/js/date.js
@@ -0,0 +1,17 @@
+/**
+ * Created by bejond on 4/17/16.
+ */
+var date = new Date();
+document.writeln(date);
+document.writeln("<br />");
+document.writeln(date.getTime());
+document.writeln("<br />");
+document.writeln(date.getFullYear());
+document.writeln("<br />");
+document.writeln(date.getDate());
+document.writeln("<br />");
+document.writeln(date.getDay());
+document.writeln("<br />");
+document.writeln(date.getMonth());
+document.writeln("<br />");
+document.writeln(date.getMinutes());
