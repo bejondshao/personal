1e8341d4b9b30077349c5fe1754b28c8d67b015f
Bejond Shao <bejond@163.com>
2016-04-25 23:31:51 +0800

    jsp tags

---
+++ java/JSP/tags/src/main/java/com/bejond/tags/HelloTag.java
@@ -0,0 +1,28 @@
+package com.bejond.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+
+/**
+ * Created by bejond on 4/25/16.
+ */
+public class HelloTag extends TagSupport {
+	@Override
+	public int doStartTag() throws JspException {
+		JspWriter jspWriter = this.pageContext.getOut();
+		try {
+			jspWriter.println("自定义标签你好");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return TagSupport.SKIP_BODY; // 跳过body, 直接结束标签
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		return super.doEndTag();
+	}
+}
---
+++ java/JSP/tags/web/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bejond
+  Date: 4/25/16
+  Time: 10:01 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="b" uri="/WEB-INF/tags.tld" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <b:helloTag/>
+  <b:reverseTag bold="true">asdfghjkl</b:reverseTag>
+  <b:reverseTag bold="false">asd fgh jkl</b:reverseTag>
+  </body>
+</html>
