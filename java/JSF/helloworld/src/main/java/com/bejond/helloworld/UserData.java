4709f50f2f4cb389cd87333eac83393e53e93cbb
Bejond Shao <bejond@163.com>
2016-05-25 16:23:54 +0800

    JSF attribute param

--- java/JSF/helloworld/src/main/java/com/bejond/helloworld/UserData.java
+++ java/JSF/helloworld/src/main/java/com/bejond/helloworld/UserData.java
@@ -2,6 +2,9 @@ package com.bejond.helloworld;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.util.Map;
 
 /**
  * Created by bejond on 2016/5/25.
@@ -17,5 +20,16 @@ public class UserData {
 
 	public void setData(String data) {
 		this.data = data;
+	}
+
+	public String showResult() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		Map<String, String> params = facesContext.getExternalContext().getRequestParameterMap();
+		data = params.get("name");
+		return "results";
+	}
+
+	public void actionListener(ActionEvent event) {
+		data = (String)event.getComponent().getAttributes().get("username");
 	}
 }
