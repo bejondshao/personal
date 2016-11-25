67d36f19e3ef1a6a66688af233d29c4ec2b81c05
Bejond Shao <bejond@163.com>
2016-11-25 16:27:06 +0800

    master System.arraycopy

---
+++ java/wholetest/src/main/java/com/bejond/wholetest/SystemTest.java
@@ -0,0 +1,23 @@
+package com.bejond.wholetest;
+
+import org.junit.Test;
+
+/**
+ * Created by Bejond on 2016/11/25.
+ */
+public class SystemTest {
+
+	@Test
+	public void testArraycopy() {
+
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = {10, 20, 30, 40, 50};
+
+		System.arraycopy(a, 1, b, 3, 2); // copy array a from index 1 to array b at index 3, 2 values. result is {10, 20, 30, 2, 3}
+
+		for (int i : b) {
+			System.out.println(i);
+		}
+	}
+
+}
