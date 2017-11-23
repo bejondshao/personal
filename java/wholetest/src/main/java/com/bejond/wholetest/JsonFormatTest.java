9cda6c0f17d7b66ee74e191a6aa8f0eefbd226ae
Bejond Shao <bejond@163.com>
2017-11-23 17:44:42 +0800

    master JsonFormatTest

---
+++ java/wholetest/src/main/java/com/bejond/wholetest/JsonFormatTest.java
@@ -0,0 +1,14 @@
+package com.bejond.wholetest;
+
+
+import org.junit.Test;
+
+public class JsonFormatTest {
+
+	@Test
+	public void testA() {
+
+		Bean bean = new Bean();
+		System.out.print(bean.toString());
+	}
+}
