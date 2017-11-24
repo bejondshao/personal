5fea64931cb502e911ba535fc273f16e30f1f2d1
Bejond Shao <bejond@163.com>
2017-11-24 11:31:16 +0800

    master netty Discard Server sample

---
+++ java/netty/sample1/src/main/java/org/bejond/netty/sample1/DiscardServerHandler.java
@@ -0,0 +1,36 @@
+package org.bejond.netty.sample1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		// Discard the received data silently.
+		// ((ByteBuf) msg).release();
+
+		ByteBuf in = (ByteBuf) msg;
+		try {
+			// System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+			while (in.isReadable()) {
+				System.out.println((char) in.readByte());
+				System.out.flush();
+			}
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
