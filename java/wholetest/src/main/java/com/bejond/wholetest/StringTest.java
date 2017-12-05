f8fb3744f9b4b61122bd9308a0957c51afa0008e
Bejond Shao <bejond@163.com>
2017-12-05 17:39:53 +0800

    master test

--- java/wholetest/src/main/java/com/bejond/wholetest/StringTest.java
+++ java/wholetest/src/main/java/com/bejond/wholetest/StringTest.java
@@ -1,11 +1,27 @@
 package com.bejond.wholetest;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 
 /**
  * Created by Bejond on 2016/7/7.
  */
 public class StringTest {
+
+	private interface I {
+
+	}
+
+	private interface I0 extends I {
+		int interface0(int a, int b);
+	}
+
+	private interface I1 extends I {
+		int interface1(double d);
+	}
+
+
 
 	@Test
 	public void testLength() {
@@ -16,5 +32,13 @@ public class StringTest {
 		System.out.println(string2.length()); // 9
 		System.out.println(string.substring(2)); // 好吗
 		System.out.println(string.substring(0, 2)); // 你还
+	}
+
+	@Test
+	public void testReplace() {
+		String str = "=\"String \"";
+		str = str.replace('"', ' ');
+		str = str.replace('=', ' ').trim();
+		System.out.println(str);
 	}
 }
